d4c6035c6222693a923a1c140f1bd24bfc7dfae0
icika-max <icikakennedy@gmail.com>
2023-01-09 14:21:15 +0300

    comppleted assgnment

--- index.js
+++ index.js
@@ -6,17 +6,7 @@ const showtime = document.getElementById('showtime')
 const description = document.getElementById('film-info')
 const ticketSpan = document.getElementById('ticket-num')
 
-// const films = document.getElementById('films')
 
-// const request = async () => {
-//     let req = await fetch("http://localhost:3000/films")
-//     let res = await req.json()
-//         res.forEach((film, i) => {
-//             const li = document.createElement('li')
-//             li.innerText = film.title
-//             films.append(li)
-//         })
-// }
 
 // request();
 const url = 'http://localhost:3000/films/8'
